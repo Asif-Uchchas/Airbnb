8484b757e09d5acb110d9df61da7f387b9711bf2
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-03-10 16:23:52 +0600

    Minor updates to fix client side rendering in server side component

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
 import React from "react";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import { IconType } from "react-icons";
@@ -68,6 +68,13 @@ export interface InputProps {
     required?: boolean
     register: UseFormRegister<FieldValues>
     errors: FieldErrors
+}
+
+export type SafeListing = Omit<
+    Listing,
+    "createdAt" 
+    > & {
+    "createdAt" : string
 }
 
 export type SafeUser = Omit<
